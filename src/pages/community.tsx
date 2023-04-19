38f5f452f661b8563736501d68a1b5e0b87f100e
Saikrishna T Bijil <saisboxintech@gmail.com>
2023-04-19 22:27:36 +0530

    image overflow at swag page

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -107,8 +107,8 @@ const Community = () => (
             src="/img/pages/community/questdb-shirt.png"
             alt="A black t-shirt with the QuestDB logo printed on the front"
             className={paCss.section_image}
-            width={400}
-            height={400}
+            width={300}
+            height={300}
           />
 
           <div className={paCss.reward}>Get a QuestDB T-shirt!</div>
